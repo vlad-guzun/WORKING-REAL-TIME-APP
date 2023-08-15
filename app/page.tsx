f8c3ebcd6a61b4cc0613422704dc9ea060b7be4a
vlad-guzun <mernapplications@gmail.com>
2023-08-15 03:24:22 -0700

    modified in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
   return (
     <div>
       <div className='flex flex-col items-center justify-center border p-7 mt-20'>
-        <h1 className='font-bold'>You must be <span className='text-pink-800 hover:text-pink-700 transition-all'>logged in</span></h1>
-        <button className='px-2 py-1 bg-pink-800 hover:bg-pink-700 transition-all text-white mt-3'>Login</button>
+        <h1 className='text-pink-800 hover:text-pink-700 transition-all font-bold'>1.Login</h1>
+        <h1 className='font-bold'><span className='text-pink-800 hover:text-pink-700 transition-all'>2.Move to https://real-time-conversation.vercel.app/chat</span></h1>
       </div>
     </div>
   )
