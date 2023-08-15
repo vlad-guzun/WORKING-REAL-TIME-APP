bc7de8cb53ad7d5ccd7286ab2c23fee3a85db03a
vlad-guzun <mernapplications@gmail.com>
2023-08-15 03:34:45 -0700

    changed description in page.tsx link is NOT WORKING, bcz of prisma

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@
 export default function Home() {
   return (
     <div>
-      <div className='flex flex-col items-center justify-center border p-7 mt-20'>
+      <div className='flex flex-col items-start justify-center border p-7 mt-20'>
         <h1 className='text-pink-800 hover:text-pink-700 transition-all font-bold'>1.Login</h1>
-        <h1 className='font-bold'><span className='text-pink-800 hover:text-pink-700 transition-all'>2.Move to https://real-time-conversation.vercel.app/chat</span></h1>
+        <h1 className='font-bold'><span className='text-pink-800 hover:text-pink-700 transition-all'>2.Copy-paste this url <span className='underline text-teal-500'>https://real-time-conversation.vercel.app/chat</span></span><span className='text-red-600'> (currently the link is not working)</span></h1>
+        <h1 className='font-bold text-pink-700'>3.Connect one device with google and another with Github and have a <span className='text-teal-500'>real time conversation</span></h1>
       </div>
     </div>
   )
